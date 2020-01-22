3a61cf98093990bed18e5071e5451a9b5c016cbd
antauren <antauren@yandex.ru>
2020-01-22 16:19:28 +0300

    Optimize comments count evaluation

--- blog/views.py
+++ blog/views.py
@@ -22,6 +22,20 @@ def serialize_post(post):
     }
 
 
+def serialize_post_optimized(post):
+    return {
+        "title": post.title,
+        "teaser_text": post.text[:200],
+        "author": post.author.username,
+        "comments_amount": post.comments_count,
+        "image_url": post.image.url if post.image else None,
+        "published_at": post.published_at,
+        "slug": post.slug,
+        "tags": [serialize_tag(tag) for tag in post.tags.all()],
+        'first_tag_title': post.tags.all()[0].title,
+    }
+
+
 def serialize_tag(tag):
     return {
         'title': tag.title,
@@ -29,21 +43,38 @@ def serialize_tag(tag):
     }
 
 
+def add_comments_count(request_posts, all_posts):
+    posts = request_posts[:]
+
+    posts_ids = [post.id for post in posts]
+
+    posts_with_comments = all_posts.filter(id__in=posts_ids).annotate(
+        comments_count=Count('comments'))
+    ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
+    count_for_id = dict(ids_and_comments)
+
+    for post in posts:
+        post.comments_count = count_for_id[post.id]
+
+    return posts
+
+
 def index(request):
     posts = Post.objects.all().prefetch_related('author')
 
-    annotated_posts = posts.annotate(likes_count=Count('likes'))
-    most_popular_posts = annotated_posts.order_by('-likes_count')[:5]
+    posts_with_likes = posts.annotate(likes_count=Count('likes'))
+    most_popular_posts = posts_with_likes.order_by('-likes_count')[:5]
+    most_popular_posts = add_comments_count(most_popular_posts, posts)
 
-    fresh_posts = posts.order_by('-published_at')
-    most_fresh_posts = fresh_posts[:5]
+    most_fresh_posts = posts.order_by('-published_at')[:5]
+    most_fresh_posts = add_comments_count(most_fresh_posts, posts)
 
     annotated_tags = Tag.objects.annotate(posts_count=Count('posts'))
     most_popular_tags = annotated_tags.order_by('-posts_count')[:5]
 
     context = {
-        'most_popular_posts': [serialize_post(post) for post in most_popular_posts],
-        'page_posts': [serialize_post(post) for post in most_fresh_posts],
+        'most_popular_posts': [serialize_post_optimized(post) for post in most_popular_posts],
+        'page_posts': [serialize_post_optimized(post) for post in most_fresh_posts],
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
     }
     return render(request, 'index.html', context)
